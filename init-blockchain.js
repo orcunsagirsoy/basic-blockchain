a96e5fa9b3d0748353a8b4cf39fea4e142908b5d
orçun sağırsoy <orcunsagirsoy@gmail.com>
2024-01-19 02:00:16 +0300

    add basic blockchain

---
+++ init-blockchain.js
@@ -0,0 +1,7 @@
+import { writeBlockchain } from './blockchain-helpers.js';
+const genesisBlock = {
+    hash: "0",
+    previousHash: null
+}
+const blockchain = [genesisBlock];
+writeBlockchain(blockchain);
